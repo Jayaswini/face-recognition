98c6a9f0d529c7bb0fdbd3ac7eaae7cb5cd23dbd
Jayaswini <jayaswini2001@gmail.com>
2020-08-24 19:57:26 +0530

    Updates

---
+++ precache-manifest.7332dd6274aa1c2a2321604aa1fe24bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22d09c9a03c81139bf66e15b6cb167a",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "110a8609cf1598563162",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6a8ce1e27bf604d2cd50",
+    "url": "/face-recognition/static/css/main.300b76dc.chunk.css"
+  },
+  {
+    "revision": "110a8609cf1598563162",
+    "url": "/face-recognition/static/js/2.a1bffe98.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.a1bffe98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8ce1e27bf604d2cd50",
+    "url": "/face-recognition/static/js/main.2c0fdfa4.chunk.js"
+  },
+  {
+    "revision": "66da7c43dfcb122d865f",
+    "url": "/face-recognition/static/js/runtime-main.252f7afd.js"
+  }
+]);
