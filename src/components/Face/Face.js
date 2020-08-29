edbfc032025c58c1724207191a6e0c88c922af93
Jayaswini <68652709+Jayaswini@users.noreply.github.com>
2020-08-29 14:09:20 +0530

    Update Face.js

--- src/components/Face/Face.js
+++ src/components/Face/Face.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Face.css';
 
-const Face = ({imageurl,box}) => {
+const Face = ({imageurl,boxes}) => {
 	return(
 		<div className='center ma'>
       <div className='absolute mt2'>
         <img id='im' alt='' src={imageurl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        {boxes.map(box => {
+        return <div key={box.topRow} className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      	})
       </div>
     </div>
   );
